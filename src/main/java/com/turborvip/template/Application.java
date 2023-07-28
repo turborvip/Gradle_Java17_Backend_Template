b80bb849b1e7ff6951e120b89eacffd2241e263f
turborvip <turborvip@gmail.com>
2023-07-28 10:59:05 +0700

    initial project

---
+++ src/main/java/com/turborvip/template/Application.java
@@ -0,0 +1,12 @@
+package com.turborvip.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Run Spring boot application!");
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/turborvip/template/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.turborvip.template;
+import org.junit.Test;
+
+public class ApplicationTest{
+
+    @Test
+    public void verifyNoExceptionThrown(){
+        System.out.println("Hello world!");
+    }
+}
